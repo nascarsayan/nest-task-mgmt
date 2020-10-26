669029ff6988018e8ea6005b9c7011fa0fdb9c33
Sayan Naskar <snaskar@microsoft.com>
2020-10-26 14:59:31 +0530

    added auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto) {
+    return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const username = await this.userRepository.validateUserPassword(
+      authCredentialsDto,
+    );
+    if (!username) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { username };
+    const accessToken = this.jwtService.sign(payload);
+
+    return { accessToken };
+  }
+}
